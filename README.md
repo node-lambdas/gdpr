c5983bdef0c1713cb06ba50ea1674bb50bcf0378
Darlan Alves <me@darlanalv.es>
2023-07-17 01:16:10 +0200

    chore: first version

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# fn-template
+# GDPR
 
-Template for new functions
+## Usage
+
+Add this snippet to your page
+
+```javascript
+import { getConsent } from 'https://gdpr.jsfn.run/index.js'
+async f() { document.body.innerHTML += await getConsent('', { color:'black', bgColor:'white' }); }
+f();
+```
--- index.mjs
+++ index.mjs
@@ -1,15 +1,17 @@
+
 function getConsent(color, bgColor) {
-  return `<div id="gdpr_dialog" role="dialog" aria-live="polite" aria-label="cookieconsent"
+  const uid = 'u4yrtkjyt6i5uy';
+  return `<div id="${uid}" role="dialog" aria-live="polite" aria-label="cookie consent"
 style="display:none;position:absolute;z-index:99999999;right:20px;bottom:20px;background-color:${bgColor};color:${color}">
 <p>We use cookies to provide our services and for analytics and marketing.
 To find out more about our use of cookies, please see our Privacy Policy.
 By continuing to browse our website, you agree to our use of cookies.
-<div style="text-align:right;">
+<div style="text-align:right">
 <button class="padding:0.5rem;margin-top:1rem;border-radius:0.25rem;border:1px solid ${color};color:${color};background:${bgColor}"
-onclick="localStorage.__gdpr_dismiss=1;document.getElementById('gdpr_dialog').remove()">Got it!</button></div>
+onclick="localStorage.__gdpr_dismiss=1;document.getElementById('${uid}').remove()">Got it!</button></div>
 </div>
 <script>
-localStorage.__gdpr_dismiss || document.getElementById('gdpr_dialog').style.display='block'
+localStorage.__gdpr_dismiss || document.getElementById('${uid}').style.display='block'
 </script>`;
 }
 export default {
