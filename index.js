27dd0ec440aa0571467630d2c9d8cbeeeeea418e
Darlan Alves <me@darlanalv.es>
2023-07-18 22:59:26 +0200

    chore: fix padding

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function getConsent(color, bgColor, url) {
   const uid = 'u4yrtkjyt6i5uy';
   return `<div id="${uid}" role="dialog" aria-live="polite" aria-label="cookie consent"
-style="display:none;position:absolute;z-index:99999999;bottom:0;margin:1rem;border-radius:0.25rem;background-color:${bgColor};color:${color}">
+style="display:none;position:absolute;z-index:99999999;bottom:0;margin:1rem;padding:1rem;border-radius:0.25rem;background-color:${bgColor};color:${color};border: 1px solid ${color}">
 <p>We use cookies to provide our services and for analytics and marketing.
 To find out more about our use of cookies, please see our ${url ? '<a href="' + url + '">Privacy Policy</a>' : 'Privacy Policy'}.
 By continuing to browse our website, you agree to our use of cookies.
@@ -10,9 +10,7 @@ By continuing to browse our website, you agree to our use of cookies.
 <button style="padding:0.5rem 1rem;border-radius:0.25rem;font-weight:bold;border:none;color:${bgColor};background:${color}"
 onclick="localStorage.__gdpr_dismiss=1;document.getElementById('${uid}').remove()">Got it!</button></div>
 </div>
-<script>
-localStorage.__gdpr_dismiss || (document.getElementById('${uid}').style.display='block')
-</script>`;
+<img src="/" onerror="localStorage.__gdpr_dismiss || (document.getElementById('u4yrtkjyt6i5uy').style.display='block')" />`;
 }
 export default {
   version: 2,
